97fe4b6806b9118a9eba4a96579034b2247155b6
Fortesa Cena <fortesacena@gmail.com>
2025-10-12 20:32:41 +0200

    Added preprocessing step

--- air_quality_analysis.py
+++ air_quality_analysis.py
@@ -3,3 +3,13 @@ import pandas as pd
 df_athens = pd.read_csv('athens.csv')
 
 df_athens['Date'] = pd.to_datetime(df_athens['Date'])
+df_athens['code'] = df_athens['code'].fillna('ANO001')
+
+cols_to_fill = [
+    "Vegitation (High)", "Wind-Speed (U)", "Wind-Speed (V)", 
+    "Dewpoint Temp", "Soil Temp", "Total Percipitation", 
+    "Vegitation (Low)", "Relative Humidity"
+]
+
+original_index = df_athens.index
+original_order = df_athens.copy()
